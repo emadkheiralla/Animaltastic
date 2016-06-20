3f54e9efc06d1e3bb673ccc4c592f15353514a2b
Emad Kheiralla <Emad Kheiralla>
2016-06-20 02:26:44 -0400

    slight change to api url

--- assets/js/hw.js
+++ assets/js/hw.js
@@ -30,7 +30,7 @@ var animals = ['bat', 'cat', 'bear', 'lion'];
 	$(document).on('click', '.animalButton', function() {
 		
         var animal = $(this).text();
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=15";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=15";
        $('#animalsView').empty();
 
        $.ajax({
